c42b9cf6973a1d11af6b4f79c0dddada366bd530
fabianGerman <fabianontiveros_7@hotmail.com>
2025-05-14 16:21:09 -0300

    segunda actualizacion

---
+++ database/migrations/2025_05_14_130853_create_precios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('precios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('prec_producto');
+            $table->foreign('prec_producto')->references('id')->onUpdate('cascade')->onDelete('cascade');
+            $table->double('prec_costo')->nullable();
+            $table->double('prec_unitario')->nullable();
+            $table->double('prec_total')->nullable();   
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('precios');
+    }
+};
---
+++ database/migrations/2025_05_14_130922_create_compras_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('comp_stock');
+            $table->foreign('comp_stock')->references('id')->on('stocks')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('comp_proveedor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('compras');
+    }
+};
